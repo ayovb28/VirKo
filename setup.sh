d5a4020688835267c9e2394e1dd74004220fcd3c
root <root@alzina-virko.virko>
2026-06-03 09:55:45 +0200

    setup.sh: instal·lar mòduls Node-RED automàticament

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 sleep 15
+
+# Instal·lar mòduls Node-RED si no estan
+docker exec virko-server-nodered-1 npm install node-red-node-mysql node-red-dashboard
+
+# ONLYOFFICE - permetre IPs privades
 docker exec virko-server-onlyoffice-1 sed -i 's/"allowPrivateIPAddress": false/"allowPrivateIPAddress": true/g' /etc/onlyoffice/documentserver/default.json
 docker exec virko-server-onlyoffice-1 sed -i 's/"allowMetaIPAddress": false/"allowMetaIPAddress": true/g' /etc/onlyoffice/documentserver/default.json
 docker restart virko-server-onlyoffice-1
+docker restart virko-server-nodered-1
