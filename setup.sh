ae7826a52a4105186551f199d9595a9fba2aeb92
root <root@alzina-virko.virko>
2026-06-03 10:55:22 +0200

    setup.sh instal·la mòduls Node-RED

--- setup.sh
+++ setup.sh
@@ -7,5 +7,7 @@ docker cp /opt/virko-server/nodered_data/flows_cred.json virko-server-nodered-1:
 # ONLYOFFICE - permetre IPs privades
 docker exec virko-server-onlyoffice-1 sed -i 's/"allowPrivateIPAddress": false/"allowPrivateIPAddress": true/g' /etc/onlyoffice/documentserver/default.json
 docker exec virko-server-onlyoffice-1 sed -i 's/"allowMetaIPAddress": false/"allowMetaIPAddress": true/g' /etc/onlyoffice/documentserver/default.json
+docker exec virko-server-nodered-1 npm install node-red-node-mysql node-red-dashboard
 docker restart virko-server-onlyoffice-1
 docker restart virko-server-nodered-1
+
