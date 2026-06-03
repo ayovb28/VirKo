097abf18e323bfc1bb194d668b9647c7e85ae388
root <root@alzina-virko.virko>
2026-06-03 11:18:05 +0200

    setup.sh sleep 60

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-sleep 15
+sleep 60
 
 # Restaurar flows_cred.json al contenidor
 docker cp /opt/virko-server/nodered_data/flows_cred.json virko-server-nodered-1:/data/flows_cred.json
 
+# Instal·lar mòduls Node-RED
+docker exec virko-server-nodered-1 npm install node-red-node-mysql node-red-dashboard
+
 # ONLYOFFICE - permetre IPs privades
 docker exec virko-server-onlyoffice-1 sed -i 's/"allowPrivateIPAddress": false/"allowPrivateIPAddress": true/g' /etc/onlyoffice/documentserver/default.json
 docker exec virko-server-onlyoffice-1 sed -i 's/"allowMetaIPAddress": false/"allowMetaIPAddress": true/g' /etc/onlyoffice/documentserver/default.json
-docker exec virko-server-nodered-1 npm install node-red-node-mysql node-red-dashboard
 docker restart virko-server-onlyoffice-1
 docker restart virko-server-nodered-1
-
